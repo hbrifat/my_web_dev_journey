2f4d4bb60257e6ffa271c28c1d817190c89846cb
hbrifat <hbrifat@gmail.com>
2024-12-14 21:46:20 +0600

    pactice problem one completedgit add .!

---
+++ Milestone_6/Module_30/practice.js
@@ -0,0 +1,19 @@
+const multiply = (num1, num2, num3) => (num1*num2*num3);
+const result = multiply(3, 4, 5);
+console.log(result);
+
+
+const printLines = `
+I am a we developer.
+I love to code.
+I love to eat biryani.
+`;
+console.log(printLines);
+
+
+
+const addNumbers = (num1, num2=10) => (num1+num2);
+const resultAddedNumbers = addNumbers(5);
+console.log(resultAddedNumbers);
+
+
